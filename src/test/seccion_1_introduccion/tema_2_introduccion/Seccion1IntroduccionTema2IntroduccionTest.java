f393382acf5941ea86d0820eb6c1d876c9f9ad60
jose <joseynene@hotmail.com>
2022-02-19 04:37:56 +0100

    Seccion_3-Lambdas_Tema_8-Lambda-Expressions - Se añaden notas, se modifican nombres y rutas y se prepara el problema

--- src/test/seccion_1_introduccion/tema_2_introduccion/Seccion1IntroduccionTema2IntroduccionTest.java
+++ src/test/seccion_1_introduccion/tema_2_introduccion/Seccion1IntroduccionTema2IntroduccionTest.java
@@ -7,9 +7,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class Seccion1introduccionTema2introduccionTest {
+public class Seccion1IntroduccionTema2IntroduccionTest {
 
     @Test
     void main() {
---
+++ src/test/seccion_3_lambdas/tema_8_lambdaExpressionss/Seccion3LambdasTema8LambdaExpressionsTest.java
@@ -0,0 +1,30 @@
+package test.seccion_3_lambdas.tema_8_lambdaExpressionss;
+
+import org.junit.jupiter.api.Test;
+import seccion_3_lambdas.tema_8_lambdaExpressionss.Seccion3LambdasTema8LambdaExpressions;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Seccion3LambdasTema8LambdaExpressionsTest {
+    private Seccion3LambdasTema8LambdaExpressions fact = new Seccion3LambdasTema8LambdaExpressions();
+
+    @Test
+    public void test_sumador () {
+        assertEquals(5, fact.obtenSumador().opera(2, 3));
+    }
+
+    @Test
+    public void test_devuelve5 () {
+        assertEquals(5, fact.devuelve5().valor());
+    }
+
+    @Test
+    public void test_inicializador() {
+        String[] array = new String[4];
+
+        fact.obtenInicializador().inicializa(array, "A");
+
+        assertArrayEquals(array, new String[] {"A","A","A","A"});
+    }
+}
